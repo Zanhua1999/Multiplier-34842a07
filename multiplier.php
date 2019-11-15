0be078bdba3e8c23340d8731ec787152a5e4eacc
Zanhua1999 <skyair886@gmail.com>
2019-11-15 13:32:56 +0100

    Uitwerking van de exercise

---
+++ multiplier.php
@@ -0,0 +1,14 @@
+<?php
+
+$a = "Eerste getal";
+$b = "Tweede getal";
+
+echo "Ik ga twee getallen met elkaar vermenigvuldigen";
+echo "\n";
+print $a;
+$line1 = readline();
+print $b;
+$line2 = readline();
+
+print_r($line1 * $line2);
+?>
